6d3cc056cffce2ba148baf9ca6668af2665d52f7
rendipilot <linahonduras@protonmail.com>
2025-04-22 09:14:25 +0700

    update create.sql

---
+++ create.sql
@@ -0,0 +1,37 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TYPE user_role AS ENUM ('admin', 'teacher', 'student');
+
+-- Table: users
+CREATE TABLE users (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    username VARCHAR(50),
+    email VARCHAR,
+    password VARCHAR(100),
+    role user_role,
+    created_at TIMESTAMP,
+    updated_at TIMESTAMP,
+    deleted_at TIMESTAMP
+);
+
+-- Table: teachers
+CREATE TABLE teachers (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    user_id UUID REFERENCES users(id),
+    active BOOLEAN,
+    created_at TIMESTAMP,
+    updated_at TIMESTAMP,
+    deleted_at TIMESTAMP
+);
+
+-- Table: histories
+CREATE TABLE histories (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    name VARCHAR(255),
+    user_id UUID REFERENCES users(id),
+    creativity_score INTEGER NOT NULL,
+    logical_score INTEGER NOT NULL,
+    complexity_score INTEGER NOT NULL,
+    level_score INTEGER NOT NULL,
+    created_at TIMESTAMP
+);
